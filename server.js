846b4aefdb241cf9f9372316e8fcd208e91075f4
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-09-27 10:03:22 +0200

    Toplist page

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const download = require("download");
 
+const TOPLIST_CHUNK = 10;
+
 var puzzles = null;
 
 setTimeout((_) => {
@@ -29,8 +31,20 @@ app.get("/", (req, res) => {
   res.send("Express Hello World!");
 });
 
-app.get("/test", (req, res) => {
-  res.send("Test Express Hello World!");
+app.get("/toplist", (req, res) => {
+  console.log("toplist", req.query);
+  const page = parseInt(req.query.page, 10) - 1;
+  const start = page * TOPLIST_CHUNK + 1;
+  const records = puzzles.slice(start, start + TOPLIST_CHUNK);
+  const bare = records.map((record) => {
+    const [rank, name, num] = record.split(",");
+    return `<tr><td>${rank}</td><td>${name}</td><td align="center">${num}</td></tr>`;
+  });
+  res.send(
+    `<table cellpadding="3" cellspacing="3" border="1"><tr><td>Rank</td><td>User</td><td>Number of puzzles</td></tr>` +
+      bare.join("\n") +
+      "</table>"
+  );
 });
 
 app.listen(port, () => {
