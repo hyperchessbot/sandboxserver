485de5ac3c46b859ceee84e2de9bc1fd3c0a6104
hyperchessbot <73081047+hyperchessbot@users.noreply.github.com>
2021-09-27 09:49:49 +0200

    Download leaderboard

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const download = require("download");
+
+var puzzles = null;
+
+setTimeout((_) => {
+  console.log("downloading leaderboard");
+  download(
+    "https://github.com/hyperchessbot/amgilp/blob/nunjucks/leaderboardfull.csv?raw=true"
+  )
+    .then((blob) => {
+      blob = blob.toString();
+      puzzles = blob.split("\n");
+      console.log(
+        "leaderboard downloaded ok, size",
+        blob.toString().length,
+        "users",
+        puzzles.length,
+        "top user",
+        puzzles[1].split(",").slice(1, 3)
+      );
+    })
+    .catch((err) => console.log("problem", err));
+}, 1000);
 
 app.get("/", (req, res) => {
   res.send("Express Hello World!");
